ea79395709b3de190b58e0c2a503609cc420c968
Vincent Foley <vfoley@gmail.com>
2013-09-23 13:16:36 -0400

    Added initial TCP work

---
+++ tcpcode/src/comp512/TCPClient.java
@@ -0,0 +1,310 @@
+package comp512;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class TCPClient {
+    public static void main(String[] args) {
+        String serverHost = args.length == 1 ? args[0] : "localhost";
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(
+            System.in));
+
+        System.out.println("Connecting to " + serverHost);
+
+        while (true) {
+            try {
+                System.out.print("> ");
+                System.out.flush();
+                String input = stdin.readLine();
+                ArrayList<String> msg = parse(input);
+
+                // Ignore empty commands
+                if (msg.isEmpty()) {
+                    continue;
+                }
+                else if (msg.get(0).equalsIgnoreCase("help")) {
+                    help(msg);
+                }
+                else {
+                    Socket socket = new Socket("localhost", 5566);
+                    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                    
+                    try {
+                        oos.writeObject(msg);
+                        //Boolean success = (Boolean)ois.readObject();
+                        Boolean success = true;
+                        if (success) {
+                            System.out.println("Command execution succeeded.");
+                        }
+                        else {
+                            System.out.println("Command execution failed.");
+                        }
+                    }
+                    catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    
+                    oos.close();
+                    socket.close();
+                }
+            }
+            catch (NullPointerException e) {
+                // Ctrl+D
+                System.exit(0);
+            }
+            catch (IOException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+        }
+
+    }
+
+    public static ArrayList<String> parse(String command) {
+        ArrayList<String> arguments = new ArrayList<String>();
+        StringTokenizer tokenizer = new StringTokenizer(command, ",");
+        String argument = "";
+        while (tokenizer.hasMoreTokens()) {
+            argument = tokenizer.nextToken();
+            argument = argument.trim();
+            arguments.add(argument);
+        }
+        return arguments;
+    }
+
+
+
+    public static void help(ArrayList<String> msg) {
+        if (msg.size() == 1) {
+            System.out.println("\nWelcome to the client interface provided to test your project.");
+            System.out.println("Commands accepted by the interface are:");
+            System.out.println("help");
+            System.out.println("newflight\nnewcar\nnewroom\nnewcustomer\nnewcusomterid\ndeleteflight\ndeletecar\ndeleteroom");
+            System.out.println("deletecustomer\nqueryflight\nquerycar\nqueryroom\nquerycustomer");
+            System.out.println("queryflightprice\nquerycarprice\nqueryroomprice");
+            System.out.println("reserveflight\nreservecar\nreserveroom\nitinerary");
+            System.out.println("nquit");
+            System.out.println("\ntype help, <commandname> for detailed info(NOTE the use of comma).");
+        }
+        else {
+            System.out.print("Help on: ");
+            switch(indexOfCommand(msg.get(1))) {
+            case 1:
+                System.out.println("Help");
+                System.out.println("\nTyping help on the prompt gives a list of all the commands available.");
+                System.out.println("Typing help, <commandname> gives details on how to use the particular command.");
+                break;
+
+            case 2:  //new flight
+                System.out.println("Adding a new Flight.");
+                System.out.println("Purpose:");
+                System.out.println("\tAdd information about a new flight.");
+                System.out.println("\nUsage:");
+                System.out.println("\tnewflight,<id>,<flightnumber>,<flightSeats>,<flightprice>");
+                break;
+
+            case 3:  //new Car
+                System.out.println("Adding a new Car.");
+                System.out.println("Purpose:");
+                System.out.println("\tAdd information about a new car location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tnewcar,<id>,<location>,<numberofcars>,<pricepercar>");
+                break;
+
+            case 4:  //new Room
+                System.out.println("Adding a new Room.");
+                System.out.println("Purpose:");
+                System.out.println("\tAdd information about a new room location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tnewroom,<id>,<location>,<numberofrooms>,<priceperroom>");
+                break;
+
+            case 5:  //new Customer
+                System.out.println("Adding a new Customer.");
+                System.out.println("Purpose:");
+                System.out.println("\tGet the system to provide a new customer id. (same as adding a new customer)");
+                System.out.println("\nUsage:");
+                System.out.println("\tnewcustomer,<id>");
+                break;
+
+
+            case 6: //delete Flight
+                System.out.println("Deleting a flight");
+                System.out.println("Purpose:");
+                System.out.println("\tDelete a flight's information.");
+                System.out.println("\nUsage:");
+                System.out.println("\tdeleteflight,<id>,<flightnumber>");
+                break;
+
+            case 7: //delete Car
+                System.out.println("Deleting a Car");
+                System.out.println("Purpose:");
+                System.out.println("\tDelete all cars from a location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tdeletecar,<id>,<location>,<numCars>");
+                break;
+
+            case 8: //delete Room
+                System.out.println("Deleting a Room");
+                System.out.println("\nPurpose:");
+                System.out.println("\tDelete all rooms from a location.");
+                System.out.println("Usage:");
+                System.out.println("\tdeleteroom,<id>,<location>,<numRooms>");
+                break;
+
+            case 9: //delete Customer
+                System.out.println("Deleting a Customer");
+                System.out.println("Purpose:");
+                System.out.println("\tRemove a customer from the database.");
+                System.out.println("\nUsage:");
+                System.out.println("\tdeletecustomer,<id>,<customerid>");
+                break;
+
+            case 10: //querying a flight
+                System.out.println("Querying flight.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain Seat information about a certain flight.");
+                System.out.println("\nUsage:");
+                System.out.println("\tqueryflight,<id>,<flightnumber>");
+                break;
+
+            case 11: //querying a Car Location
+                System.out.println("Querying a Car location.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain number of cars at a certain car location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tquerycar,<id>,<location>");
+                break;
+
+            case 12: //querying a Room location
+                System.out.println("Querying a Room Location.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain number of rooms at a certain room location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tqueryroom,<id>,<location>");
+                break;
+
+            case 13: //querying Customer Information
+                System.out.println("Querying Customer Information.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain information about a customer.");
+                System.out.println("\nUsage:");
+                System.out.println("\tquerycustomer,<id>,<customerid>");
+                break;
+
+            case 14: //querying a flight for price
+                System.out.println("Querying flight.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain price information about a certain flight.");
+                System.out.println("\nUsage:");
+                System.out.println("\tqueryflightprice,<id>,<flightnumber>");
+                break;
+
+            case 15: //querying a Car Location for price
+                System.out.println("Querying a Car location.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain price information about a certain car location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tquerycarprice,<id>,<location>");
+                break;
+
+            case 16: //querying a Room location for price
+                System.out.println("Querying a Room Location.");
+                System.out.println("Purpose:");
+                System.out.println("\tObtain price information about a certain room location.");
+                System.out.println("\nUsage:");
+                System.out.println("\tqueryroomprice,<id>,<location>");
+                break;
+
+            case 17:  //reserve a flight
+                System.out.println("Reserving a flight.");
+                System.out.println("Purpose:");
+                System.out.println("\tReserve a flight for a customer.");
+                System.out.println("\nUsage:");
+                System.out.println("\treserveflight,<id>,<customerid>,<flightnumber>");
+                break;
+
+            case 18:  //reserve a car
+                System.out.println("Reserving a Car.");
+                System.out.println("Purpose:");
+                System.out.println("\tReserve a given number of cars for a customer at a particular location.");
+                System.out.println("\nUsage:");
+                System.out.println("\treservecar,<id>,<customerid>,<location>,<nummberofCars>");
+                break;
+
+            case 19:  //reserve a room
+                System.out.println("Reserving a Room.");
+                System.out.println("Purpose:");
+                System.out.println("\tReserve a given number of rooms for a customer at a particular location.");
+                System.out.println("\nUsage:");
+                System.out.println("\treserveroom,<id>,<customerid>,<location>,<nummberofRooms>");
+                break;
+
+            case 20:  //reserve an Itinerary
+                System.out.println("Reserving an Itinerary.");
+                System.out.println("Purpose:");
+                System.out.println("\tBook one or more flights.Also book zero or more cars/rooms at a location.");
+                System.out.println("\nUsage:");
+                System.out.println("\titinerary,<id>,<customerid>,<flightnumber1>....<flightnumberN>,<LocationToBookCarsOrRooms>,<NumberOfCars>,<NumberOfRoom>");
+                break;
+
+
+            case 21:  //quit the client
+                System.out.println("Quitting client.");
+                System.out.println("Purpose:");
+                System.out.println("\tExit the client application.");
+                System.out.println("\nUsage:");
+                System.out.println("\tquit");
+                break;
+
+            case 22:  //new customer with id
+                System.out.println("Create new customer providing an id");
+                System.out.println("Purpose:");
+                System.out.println("\tCreates a new customer with the id provided");
+                System.out.println("\nUsage:");
+                System.out.println("\tnewcustomerid, <id>, <customerid>");
+                break;
+
+            default:
+                System.out.println("The interface does not support this command.");
+                break;
+            }
+        }
+    }
+
+    public static int indexOfCommand(String command) {
+        ArrayList<String> commands = new ArrayList<String>() {{
+            add("help");
+            add("newflight");
+            add("newcar");
+            add("newroom");
+            add("newcustomer");
+            add("deleteflight");
+            add("deletecar");
+            add("deleteroom");
+            add("deletecustomer");
+            add("queryflight");
+            add("querycar");
+            add("queryroom");
+            add("querycustomer");
+            add("queryflightprice");
+            add("querycarprice");
+            add("queryroomprice");
+            add("reserveflight");
+            add("reservecar");
+            add("reserveroom");
+            add("itinerary");
+            add("quit");
+            add("newcustomerid");
+        }};
+        return commands.indexOf(command);
+    }
+
+}
---
+++ tcpcode/src/comp512/TCPServer.java
@@ -0,0 +1,124 @@
+package comp512;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class HostPort {
+    public String host;
+    public int port;
+    
+    public HostPort(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+}
+
+public class TCPServer {
+    private Map<String, HostPort> backends;
+    
+    public TCPServer() {
+        backends = new HashMap<String, HostPort>();
+    }
+    
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Usage: TCPServer car=<host>:<port> flight=<host>:<port> hotel=<host>:<port>");
+            System.exit(1);
+        }
+        
+        TCPServer obj = new TCPServer();
+        obj.populateBackends(args);
+        
+        ExecutorService executor = Executors.newFixedThreadPool(16);
+        ServerSocket server;
+        try {
+            server = new ServerSocket(5566);
+            while (true) {
+                // Accept connection from a new client
+                final Socket connection;
+                try {
+                    connection = server.accept();
+                    System.out.println("Connection from "
+                        + connection.getRemoteSocketAddress());
+                }
+                catch (IOException e) {
+                    System.err.println("Error in server.accept(): "
+                        + e.getMessage());
+                    continue;
+                }
+
+                ArrayList<String> msg = obj.readMessage(connection);
+                System.out.println(msg);
+            }
+        }
+        catch (IOException e) {
+            System.err.println("Cannot create a new socket on port 5566.");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Add the backends with their associated host/port information
+     * to the backends map.
+     * @param args array of mappings of the form: backend=host:port
+     */
+    public void populateBackends(String[] args) {
+        for (String arg: args) {
+            String[] keyValue = arg.split("=");
+            String[] hostPort = keyValue[1].split(":");
+            backends.put(keyValue[0], 
+                new HostPort(hostPort[0], Integer.parseInt(hostPort[1])));
+        }
+    }
+    
+    
+    /**
+     * Read an ArrayList<String> from a client connection.
+     * @param connection The client connection.
+     * @return The array list if successful, null otherwise.
+     */
+    public ArrayList<String> readMessage(Socket connection) {
+        try {
+            ObjectInputStream clientInput = new ObjectInputStream(
+                connection.getInputStream()
+                );
+            ArrayList<String> msg = (ArrayList<String>)clientInput.readObject();
+            return msg;
+        }
+        catch (ClassNotFoundException e) {
+            System.err.println(e.toString());
+            return null;
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+            return null;
+        }
+    }
+    
+    
+    public void sendResponse(Socket connection, Boolean b) {
+        try {
+            ObjectOutputStream clientOutput = new ObjectOutputStream(
+                connection.getOutputStream()
+                );
+            clientOutput.writeObject(b);
+            clientOutput.flush();
+            clientOutput.close();
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+}
